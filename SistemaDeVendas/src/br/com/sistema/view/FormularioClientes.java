e3ffa0f1823e1a515a7ea967b6fbe0d03caa7a2f
Ana Nunes <ananunes.dev@gmail.com>
2024-08-13 21:39:23 -0300

    tentativa com tabbed panel

--- SistemaDeVendas/src/br/com/sistema/view/FormularioClientes.java
+++ SistemaDeVendas/src/br/com/sistema/view/FormularioClientes.java
@@ -5,6 +5,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTabbedPane;
 
 public class FormularioClientes extends JFrame {
 
@@ -32,11 +37,29 @@ public class FormularioClientes extends JFrame {
 	 */
 	public FormularioClientes() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.BLACK);
+		panel.setBounds(0, 0, 784, 70);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Cadastro de Clientes");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 22));
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setBounds(0, 0, 784, 70);
+		panel.add(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(0, 81, 784, 403);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
 	}
-
 }
